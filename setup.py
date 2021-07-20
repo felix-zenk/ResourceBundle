c376c5712de492f9e129d7269bf2dfbcdbbc8eaf
Felix Zenk <felix.zenk@student.uni-luebeck.de>
2021-07-21 00:05:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )

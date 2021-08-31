4903dbb6bcee801b8ad64151037b099ef8d7c97b
Felix Zenk <felix.zenk@student.uni-luebeck.de>
2021-08-31 22:24:28 +0200

    Fixed an encoding issue

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ import setuptools
 import re
 from ResourceBundle import __version__, __author__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
     long_description = long_description.replace("[![PyPI version](https://img.shields.io/badge/pypi-v1.0.2-yellow)]("
                                                 "https://pypi.org/project/ResourceBundle)", "")  # Don't display on PyPI
-    long_description = long_description.replace("├", "|").replace("└", "|").replace("─", "-")  # PyPI compatibility
-
 
 url = "https://github.com/felix-zenk/ResourceBundle"
 

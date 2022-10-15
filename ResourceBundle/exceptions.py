05e1bdc0d569f3850eb55cdeba5bb6b3b6ed9b60
Felix <felix.zenk@web.de>
2022-10-15 02:24:52 +0200

    Update version

--- ResourceBundle/exceptions.py
+++ ResourceBundle/exceptions.py
@@ -1,23 +1,20 @@
-class MissingResourceBundleError(LookupError):
-
-    def __init__(self, bundle_name):
-        """
-        Error that indicates that the BasicResourceBundle is missing.
-        :param bundle_name: The name of the BasicResourceBundle
-        :type bundle_name: str
-        """
-        super(MissingResourceBundleError, self).__init__("The BasicResourceBundle {} does not exist".format(bundle_name))
+class ResourceBundleError(LookupError):
+    pass
 
 
-class NotInResourceBundleError(LookupError):
+class MalformedResourceBundleError(ResourceBundleError):
+    """
+    Error that indicates that a ResourceBundle is malformed.
+    """
+
+
+class NotInResourceBundleError(ResourceBundleError):
 
     def __init__(self, bundle_name: str, key: str):
         """
-        Error that is raised when a key could not be found in a BasicResourceBundle.
-        :param bundle_name: The name of the BasicResourceBundle
-        :type bundle_name: str
-        :param key: The key that could not be found
-        :type key: str
+        Error that is raised when a key could not be found in a ResourceBundle.
+
+        :param str bundle_name: The name of the ResourceBundle
+        :param str key: The key that could not be found
         """
-        super(NotInResourceBundleError, self).__init__("Can't find key {} in bundle {}"
-                                                       .format(key, bundle_name))
+        super(NotInResourceBundleError, self).__init__(f"Can't find key {key} in bundle {bundle_name}!")

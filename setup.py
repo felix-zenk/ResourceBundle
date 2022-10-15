c76b85fc9d94011e0dfa717e78e8db21d61b50a7
Felix <felix.zenk@web.de>
2022-10-15 02:13:04 +0200

    Rewrite to simpler code

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 import setuptools
-import re
 from ResourceBundle import __version__, __author__, __email__
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
-
-    # Don't display badge on PyPI
-    long_description = re.sub(r"\[!\[PyPI version.*\d+\.\d+\.\d+.*yellow\)\]\(.*project/ResourceBundle\)", "",
-                              long_description)
 
 homepage = "https://felix-zenk.github.io/projects/ResourceBundle"
 source = "https://github.com/felix-zenk/ResourceBundle"
@@ -18,18 +13,24 @@ setuptools.setup(
     version=__version__,
     author=__author__,
     author_email=__email__,
-    description="ResourceBundle is a module that manages resource handling where different resources are needed " +
-                "depending on the current locale",
+    description="ResourceBundle is a module that manages internationalization of string resources",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=homepage,
     project_urls={
-        "Bug Reports": source+"/issues/new?labels=bug&template=bug_report.md&title=%5BBUG%5D%3A+",
+        "Bug Reports": f"{source}/issues",
         "Source": source
     },
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(".", exclude=["tests", "tests.*"]),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Utilities',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],

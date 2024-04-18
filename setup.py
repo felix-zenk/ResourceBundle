4431ce9fba330ce73c4c4a779d6fb839fca04578
Felix Zenk <felix.zenk@web.de>
2024-04-18 03:38:22 +0200

    Support Python 3.5+

--- setup.py
+++ setup.py
@@ -25,11 +25,6 @@ setuptools.setup(
     packages=setuptools.find_packages(".", include=["ResourceBundle*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -43,5 +38,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    python_requires='>=3'
+    python_requires='>=3.5'
 )
